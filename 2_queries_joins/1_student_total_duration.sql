2c054372f713cd1df2cc19c7d0d30dd83f258e45
Rina Inada <indrn@icloud.com>
2023-07-29 21:05:06 +0000

    Add 3_queries

--- 2_queries_joins/1_student_total_duration.sql
+++ 2_queries_joins/1_student_total_duration.sql
@@ -6,6 +6,6 @@
 
 SELECT SUM(duration) as total_duration
 FROM assignment_submissions
-INNER JOIN students
-ON assignment_submissions.student_id = students.id
+JOIN students
+ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
